40ff43b90003c08ce01edc4b0cca285cf668c106
Chester Wong <cwong5@gmail.com>
2018-08-11 19:03:12 -0400

    Initial Commit

---
+++ ToDoey/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDoey
+//
+//  Created by Chester Wong on 2018-08-11.
+//  Copyright © 2018 CWC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
