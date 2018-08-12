88cac3f7d1386741bf8d1d3b31445830db93204e
Chester Wong <cwong5@gmail.com>
2018-08-12 12:26:15 -0400

    First commit

--- ToDoey/ToDoListVIewController.swift
+++ ToDoey/ToDoListVIewController.swift
@@ -16,5 +16,35 @@ class ToDoListVIewController: UITableViewController {
     }
 
 
+    let itemArray = ["First item", "Second item", "third item"]
+    
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print (itemArray[indexPath.row])
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType != .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+
+        }
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+    }
+    
 }
 
